c46b2cf63920d9506485b5ed33f56e85b1d3a756
emmanuelmwahara <mwaharae@gmail.com>
2022-07-16 23:57:16 +0200

    added more controls

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,7 @@ class _HomeState extends State<Home> {
   final database = FirebaseDatabase.instance.reference().child('test');
   bool isOn = false;
   int state = 0;
+  int hornState = 0;
 
   void _toggleState() {
     if (state == 0) {
@@ -28,6 +29,12 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void _horn() {
+    database.update({
+      "horn": hornState,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +42,110 @@ class _HomeState extends State<Home> {
         title: const Text("Remote forklift"),
         centerTitle: true,
       ),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: _toggleState,
-            child: Text((state == 1) ? "on" : "off"),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ColoredBox(
+            color: Colors.blueAccent.shade100,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_upward_outlined,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_downward_outlined,
+                      size: 30,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_left_outlined,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_right_outlined,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+          GestureDetector(
+            onTap: () {
+              print('tap');
+            },
+            onTapUp: (Ta) {},
+            child: const Text('try'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('HORN'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: _toggleState,
+              child: Text((state == 1) ? "on" : "off"),
+            ),
+          ),
+          ColoredBox(
+            color: Colors.blue.shade50,
+            child: Column(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_upward_outlined,
+                    size: 30,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_downward_outlined,
+                    size: 30,
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_left_outlined,
+                        size: 30,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_right_outlined,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
